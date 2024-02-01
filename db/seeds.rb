5a2cc2c989c4340fb3ceda4ec16351c9431dc202
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-02-01 20:58:34 +0100

    Generation des controlleurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create(name: "Epicure", category: "french", address: "Paris", phone_number: "0606060606")
+Restaurant.create(name: "La bonne franquete", category: "french", address: "Janvry", phone_number: "0707070707")
+Restaurant.create(name: "L'ile aux crepes", category: "breton", address: "Gif", phone_number: "0808080808")
+Restaurant.create(name: "Ta sushi", category: "japanese", address: "Verrières le buisson", phone_number: "0909090909")
+Restaurant.create(name: "Frères Tang", category: "chinese", address: "Paris", phone_number: "0505050505")
+
+puts "Finished"
